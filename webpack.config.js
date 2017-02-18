7503cf38727a4ba583d2dd637abdfa4e99f6efd7
Esa-Matti Suuronen <esa-matti@suuronen.org>
2017-02-18 22:57:53 +0200

    Pass git commit info to bundles

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 var webpack = require("webpack");
 var path = require("path");
+var {execSync} = require("child_process");
+
+var gitRev = execSync("git rev-parse HEAD").toString();
+var gitMessage = execSync("git log -1 --pretty=%B").toString();
+var gitDate = execSync("git log -1 --format=%cd ").toString();
 
 var config = {
     entry: "./index.js",
@@ -21,6 +26,9 @@ var config = {
     plugins: [
         new webpack.DefinePlugin({
             "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+            "GIT_COMMIT_REV": JSON.stringify(gitRev),
+            "GIT_COMMIT_MESSAGE": JSON.stringify(gitMessage),
+            "GIT_COMMIT_DATE": JSON.stringify(gitDate),
         }),
     ],
 };

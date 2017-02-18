bfee4d8b912836b1867a9cdbdd694e96dd50805b
Esa-Matti Suuronen <esa-matti@suuronen.org>
2017-02-18 23:02:40 +0200

    Add example of git data
    Just log those.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import React from "react"; // must be in scope when using JSX
 import ReactDOM from "react-dom";
 
+console.log("git date", GIT_COMMIT_DATE);
+console.log("git message", GIT_COMMIT_MESSAGE);
+console.log("git rev", GIT_COMMIT_REV);
 
 function HelloWorld() {
     return (

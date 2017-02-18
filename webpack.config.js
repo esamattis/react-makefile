665a6ca8c3448f8caed953cc418e65f1b1ec58c9
Esa-Matti Suuronen <esa-matti@suuronen.org>
2017-02-18 23:02:17 +0200

    fixup

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ var path = require("path");
 var {execSync} = require("child_process");
 
 var gitRev = execSync("git rev-parse HEAD").toString();
-var gitMessage = execSync("git log -1 --pretty=%B").toString();
+var gitMessageShort = execSync("git log -1 --pretty=%s").toString();
+var gitMessageFull = execSync("git log -1 --pretty=%B").toString();
 var gitDate = execSync("git log -1 --format=%cd ").toString();
 
 var config = {
@@ -27,7 +28,8 @@ var config = {
         new webpack.DefinePlugin({
             "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
             "GIT_COMMIT_REV": JSON.stringify(gitRev),
-            "GIT_COMMIT_MESSAGE": JSON.stringify(gitMessage),
+            "GIT_COMMIT_MESSAGE": JSON.stringify(gitMessageShort),
+            "GIT_COMMIT_MESSAGE_FULL": JSON.stringify(gitMessageFull),
             "GIT_COMMIT_DATE": JSON.stringify(gitDate),
         }),
     ],
